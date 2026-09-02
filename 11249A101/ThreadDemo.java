a7deaa24b8cb8953a7b04b54c5344c38981aa5cc
GChandraShekar101 <11249A101@kanchiuniv.ac.in>
2026-09-02 11:06:43 +0530

    Add files via upload

---
+++ 11249A101/ThreadDemo.java
@@ -0,0 +1,28 @@
+class MyThread extends Thread{
+    public void run() {
+        for(int i=1;i<=5;i++){
+            System.out.println("child Thread:"+i);
+            try{
+                Thread.sleep(500);
+            }
+            catch(InterruptedException e){
+                System.out.println(e);
+            }
+        }
+    }
+}
+public class ThreadDemo{
+    public static void main(String[] args){
+        MyThread t=new MyThread();
+        t.start();
+        for(int i = 1 ; i <= 5; i++){
+            System.out.println("Main Thread:"+i);
+            try{
+                Thread.sleep(500);
+            }
+            catch(InterruptedException e){
+                System.out.println(e);
+            }
+        }
+    }
+}
